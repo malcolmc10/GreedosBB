6991ec134beac0b9d733dcf2edb4271ea9da6ee2
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-01 12:50:10 -0400

    created my footer component

---
+++ greedosbb/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+import {Link} from 'react-router-dom';
+
+
+
+export default function Footer() {
+    return(
+        <footer>
+            <p>This project is made by Colin Malcolm using the SWAPI API.</p>
+        </footer>
+    )
+}
