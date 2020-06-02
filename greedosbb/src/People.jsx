f878926f52ab1b1c31d25e5ed1297fc956bcd420
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-02 17:27:39 -0400

    added css

--- greedosbb/src/People.jsx
+++ greedosbb/src/People.jsx
@@ -3,7 +3,7 @@ import "./People.css";
 import {Link} from 'react-router-dom';
 
 export default function People(props) {
-    // functional components need to be configured.
+    
     return (
         // I want to add an image to the characters below. based on affiliation.
         
