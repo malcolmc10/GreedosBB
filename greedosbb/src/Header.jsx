160f06b583b1a26353d7447853a0fbcb64f6b2a5
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-04 16:21:54 -0400

    css changes

--- greedosbb/src/Header.jsx
+++ greedosbb/src/Header.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import "./Header.css";
 import {Link} from 'react-router-dom';
-import logo from "./Greedos-Bounty-Board-Logo.png";
+import Logo from "./Logo";
 
 // below is the code for the header. includes link back to the home page.
 export default function Header() {
     return(
         <header>
-             
+             <div className="logo">
+             <Logo />
+            </div>
             
             <nav>
-            
-            
-                
+                <div className="link">
                 <Link to='/' className="topBar">
                     
                     <p>Home</p>
                     
                 </Link>
-                
+                </div>
                 
             </nav>
             
---
+++ greedosbb/src/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Logo() {
+    return (
+        <div>
+            <img src={require("./Greedos-Bounty-Board-Logo.png")} />
+        </div>
+    )
+}
+
